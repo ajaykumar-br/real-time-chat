b15f7e05a80155ce22e2474cc9922cfddc7fb551
Ajay Kumar B R <ajraj86@gmail.com>
2024-11-25 15:35:46 -0500

    added store

---
+++ src/InMemoryStore.ts
@@ -0,0 +1,56 @@
+import { Chat, Store, UserId } from "./store/Store";
+
+let globalChatId = 0;
+
+interface room {
+  roomId: string;
+  chats: Chat[];
+}
+
+export class InMemoryStore implements Store {
+  private store: Map<string, room>;
+  constructor() {
+    this.store = new Map<string, room>();
+  }
+
+  initRoom(roomId: string) {
+    this.store.set(roomId, { roomId, chats: [] });
+  }
+
+  getChats(roomId: string, limit: number, offset: number) {
+    const room = this.store.get(roomId);
+    if (!room) {
+      return [];
+    }
+    return room.chats
+      .reverse()
+      .slice(0, offset)
+      .slice(-1 * limit);
+  }
+
+  addChat(userId: UserId, roomId: string, name: string, message: string) {
+    const room = this.store.get(roomId);
+    if (!room) return [];
+
+    room.chats.push({
+      id: (globalChatId++).toString(),
+      userId,
+      name,
+      message,
+      upvotes: [],
+    });
+  }
+
+  upvote(userId: UserId, roomId: string, chatId: string) {
+    const room = this.store.get(roomId);
+    if (!room) {
+      return;
+    }
+
+    const chat = room.chats.find(({ id }) => id === chatId);
+
+    if (chat) {
+      chat.upvotes.push(userId);
+    }
+  }
+}
